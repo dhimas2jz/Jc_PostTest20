7c5d8c094cabe8c917e4de6dfd4552da23d0f079
hekilledhim <dhimzmarwahz@gmail.com>
2022-12-03 12:40:07 +0700

    first

---
+++ src/main/java/com/dhimas/belajar/belajarcucumber3/App.java
@@ -0,0 +1,13 @@
+package com.dhimas.belajar.belajarcucumber3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/dhimas/belajar/belajarcucumber3/RunnerPostTest20.java
@@ -0,0 +1,13 @@
+package com.dhimas.belajar.belajarcucumber3;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions (
+		features = "src/test/java/com/dhimas/belajar/belajarcucumber3",
+		glue=""
+	)
+
+public class RunnerPostTest20 extends AbstractTestNGCucumberTests {
+
+}
